a570b79db7b45a260514c87aaef584344558aa6e
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-08-31 13:08:51 +0300

    add js file

---
+++ index.js
@@ -0,0 +1,16 @@
+var myHeaders = new Headers();
+myHeaders.append("X-RapidAPI-Key", "c1d9267467msh5e96a7a8932441bp1ccd00jsn745bf1fe6106");
+myHeaders.append("X-RapidAPI-Host", "zillow56.p.rapidapi.com");
+
+var raw = "";
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://zillow56.p.rapidapi.com/search?location=houston%2C%20tx", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
