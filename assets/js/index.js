00d3a3b8c94d33cbaf430c626573b40618abd599
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-09-01 10:41:11 +0300

    add html styling and add images

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+
+const messageAlert = document.getElementsByClassName("btn btn--green")
+
+
 var myHeaders = new Headers();
 myHeaders.append("X-RapidAPI-Key", "c1d9267467msh5e96a7a8932441bp1ccd00jsn745bf1fe6106");
 myHeaders.append("X-RapidAPI-Host", "zillow56.p.rapidapi.com");
@@ -14,3 +18,5 @@ fetch("https://zillow56.p.rapidapi.com/search?location=houston%2C%20tx", request
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+
+
